6c31d65d93080d31bf7bd1e18579df63a111c0c6
Joël Israël <stadja@gmail.com>
2020-09-30 14:27:11 +0200

    On ajoute les adresses de déploiements sur le git

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,23 @@
+const env = require('dotenv').config()
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = env.parsed.ACCOUNT_MENMONIC;
+
 module.exports = {
 
   networks: {
-
     development: {
       host: "0.0.0.0",
       port: 7545,
       network_id: "5777"
+    },
+
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+env.parsed.INFURA_API_KEY)
+      },
+      network_id: 3
     }
-
-
   }
 
 };
